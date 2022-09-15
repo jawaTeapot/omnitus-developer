82080a4b976062dfa115d71d18eeda553084e759
Alex Insayt <mr.insayt@gmail.com>
2022-09-15 16:50:07 +0300

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ function buildFonts() {
 }
 
 function buildEjs() {
-    return gulp.src("./templates/*.ejs")
+    return gulp.src("./templates/*.*")
         .pipe(ejs({
             msg: "Hello Gulp!"
         }))
