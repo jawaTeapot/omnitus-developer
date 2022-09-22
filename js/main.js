e1f9375801d62f173a59cec297eea189f42c90ed
jawaTeapot <jawa_teapot@bk.ru>
2022-09-22 19:27:14 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -57,4 +57,43 @@ $(document).ready(function() {
         $('.tab-content').hide();
         $('.' + $(this).data().tab).show();
     });
+
+    ////// МЕНЮ //////
+
+    $(function() {
+        const $menu_popup = $('.burger-menu__popup');
+        $(".burger-menu__trigger").click(function(){
+            $('body').addClass('body_pointer');
+            $menu_popup.show(0);
+            $menu_popup.animate(
+                {left: parseInt($menu_popup.css('left'),10) === 0 ? -$menu_popup.outerWidth() : 0},
+                300
+            );
+            return false;
+        });
+        $(".burger-menu__close").click(function(){
+            $('body').removeClass('body_pointer');
+            $menu_popup.animate(
+                {left: parseInt($menu_popup.css('left'),10) === 0 ? -$menu_popup.outerWidth() : 0},
+                300,
+                function(){
+                    $menu_popup.hide(0);
+                }
+            );
+            return false;
+        });
+        $(document).on('click', function(e){
+            if (!$(e.target).closest('.burger-menu__popup').length){
+                $('body').removeClass('body_pointer');
+                $menu_popup.animate(
+                    {left: parseInt($menu_popup.css('left'),10) === 0 ? -$menu_popup.outerWidth() : 0},
+                    300,
+                    function(){
+                        $menu_popup.hide(0);
+                    }
+                );
+            }
+        });
+    });
+
 });
